09ba565cf7de0ff002cb9319e505ac1fdf8531d0
Matt Weichel <maweiche@gmail.com>
2023-11-06 10:16:50 +0100

    added main branch with readme on how to begin tutorial

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['localhost', 'media.tenor.com', 'static.seekingalpha.com'],
-    },
-}
-
-module.exports = nextConfig
