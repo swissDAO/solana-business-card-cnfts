63e8c77cab31bce9e3fd6df729217410b4d85f31
Matt Weichel <maweiche@gmail.com>
2023-11-06 17:51:05 +0100

    added solution for mint api route and included mint cNFT button on front-end

--- app/page.tsx
+++ app/page.tsx
@@ -457,6 +457,16 @@ const Page: React.FC = () => {
               website={website}
             />
           </div>
+          {selectedSvg != "" && (
+            <div className="flex justify-center">
+              <button
+                className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => convertAndSubmit()}
+              >
+                Create cNFT
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
